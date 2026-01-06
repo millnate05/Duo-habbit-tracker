700ad5bbe84faaf617d9612dee568a1261a5c038
millnate05 <millnate05@gmail.com>
2026-01-06 14:56:11 -0800

    Update homepage.tsx

--- src/app/home/homepage.tsx
+++ src/app/home/homepage.tsx
@@ -1,1 +1,25 @@
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+
+export default function HomeTab() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const goHome = () => {
+    if (pathname !== "/") {
+      router.push("/");
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={goHome}
+      className="cursor-pointer select-none"
+    >
+      Home
+    </button>
+  );
+}
 
