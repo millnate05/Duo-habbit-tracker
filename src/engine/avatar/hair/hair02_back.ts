a56c4e8f0405c086ac6538c1d48ba8248d75837d
millnate05 <millnate05@gmail.com>
2026-01-06 19:05:37 -0800

    Update hair02_back.ts

--- src/engine/avatar/hair/hair02_back.ts
+++ src/engine/avatar/hair/hair02_back.ts
@@ -1,1 +1,11 @@
+export const hair02_back = `
+<g id="hair02_back">
+  <path class="ol" d="M164 182
+    C176 118 214 92 256 92
+    C298 92 336 118 348 182
+    C332 162 308 154 256 154
+    C204 154 180 162 164 182 Z"
+    fill="var(--hair)"/>
+</g>
+`;
 
