8b56c722efa59a47a0e44a64d72781944bcf2bad
millnate05 <millnate05@gmail.com>
2026-01-06 19:06:46 -0800

    Update glasses01.ts

--- src/engine/avatar/accessories/glasses01.ts
+++ src/engine/avatar/accessories/glasses01.ts
@@ -1,1 +1,8 @@
+export const glasses01 = `
+<g id="glasses01">
+  <rect class="ol" x="176" y="212" width="84" height="56" rx="22" fill="rgba(255,255,255,0.12)"/>
+  <rect class="ol" x="252" y="212" width="84" height="56" rx="22" fill="rgba(255,255,255,0.12)"/>
+  <path class="ol noFill" d="M260 240 L260 240"/>
+</g>
+`;
 
