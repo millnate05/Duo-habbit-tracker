9629d9495d2749d5776522677888f31954b2c795
millnate05 <millnate05@gmail.com>
2026-01-06 19:04:11 -0800

    Update brows01.ts

--- src/engine/avatar/face/brows01.ts
+++ src/engine/avatar/face/brows01.ts
@@ -1,1 +1,7 @@
+export const brows01 = `
+<g id="brows01">
+  <path class="ol noFill" d="M196 190 C220 176 244 176 260 190"/>
+  <path class="ol noFill" d="M252 190 C268 176 292 176 316 190"/>
+</g>
+`;
 
