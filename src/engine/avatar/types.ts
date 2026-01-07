7fc18b25a1a82ebc80cee2a90946fb34070a90e0
millnate05 <millnate05@gmail.com>
2026-01-07 15:04:09 -0800

    Update types.ts

--- src/engine/avatar/types.ts
+++ src/engine/avatar/types.ts
@@ -1,25 +1,40 @@
-export type SkinTone = "s1" | "s2" | "s3" | "s4" | "s5" | "s6";
-export type HairColor = "hc1" | "hc2" | "hc3" | "hc4";
+// src/engine/avatar/types.ts
 
-export type OutfitColor = "o1" | "o2";
+export type SkinToneId = "s1" | "s2" | "s3" | "s4" | "s5" | "s6";
+export type HairColorId = "hc1" | "hc2" | "hc3" | "hc4";
+
+// These should match the keys in PARTS
+export type HeadId = "head01";
+export type EyesId = "eyes01" | "eyes02";
+export type BrowsId = "brows01" | "brows02";
+export type NoseId = "nose01";
+export type MouthId = "mouth01" | "mouth02";
+export type HairId = "hair01" | "hair02";
+export type OutfitTopId = "top01" | "top02";
+export type AccessoryId = "glasses01" | "none";
 
 export type AvatarRecipe = {
-  style: "bust_v1";
+  skinTone: SkinToneId;
+  hairColor: HairColorId;
+  head: HeadId;
+  eyes: EyesId;
+  brows: BrowsId;
+  nose: NoseId;
+  mouth: MouthId;
+  hair: HairId;
+  outfitTop: OutfitTopId;
+  accessory: AccessoryId;
+};
 
-  skin: SkinTone;
-
-  head: "head01";
-
-  eyes: "eyes01" | "eyes02";
-  brows: "brows01" | "brows02";
-  nose: "nose01";
-  mouth: "mouth01" | "mouth02";
-
-  hair: "hair01" | "hair02";
-  hairColor: HairColor;
-
-  outfitTop: "top01" | "top02";
-  outfitColor: OutfitColor;
-
-  accessory: "none" | "glasses01";
+export const DEFAULT_AVATAR: AvatarRecipe = {
+  skinTone: "s2",
+  hairColor: "hc2",
+  head: "head01",
+  eyes: "eyes01",
+  brows: "brows01",
+  nose: "nose01",
+  mouth: "mouth01",
+  hair: "hair01",
+  outfitTop: "top01",
+  accessory: "none",
 };
