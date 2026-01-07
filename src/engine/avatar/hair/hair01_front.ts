69c563440a1c8a78d4adea0cf29470357e7eaa6f
millnate05 <millnate05@gmail.com>
2026-01-06 19:05:29 -0800

    Update hair01_front.ts

--- src/engine/avatar/hair/hair01_front.ts
+++ src/engine/avatar/hair/hair01_front.ts
@@ -1,1 +1,19 @@
+export const hair01_front = `
+<g id="hair01_front">
+  <!-- temples -->
+  <path class="ol" d="M174 240 C172 214 186 196 208 190 C196 210 196 226 200 248 C188 252 178 250 174 240 Z" fill="var(--hair)"/>
+  <path class="ol" d="M338 240 C340 214 326 196 304 190 C316 210 316 226 312 248 C324 252 334 250 338 240 Z" fill="var(--hair)"/>
+
+  <!-- hairline mass -->
+  <path class="ol" d="M176 188
+    C190 126 222 104 256 104
+    C290 104 322 126 336 188
+    C316 168 292 160 256 160
+    C220 160 196 168 176 188 Z"
+    fill="var(--hair)"/>
+
+  <!-- highlight -->
+  <path class="ol noFill" d="M212 140 C232 122 244 120 256 120 C276 120 292 128 306 140" stroke="var(--highlight)"/>
+</g>
+`;
 
